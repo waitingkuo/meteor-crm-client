ebc810f4268970fc4282d43b7554c70a04595fa9
waitingkuo <waitingkuo0527@gmail.com>
2014-10-19 21:51:58 +0800

    version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'waitingkuo:meteor-crm-client',
     summary: 'Meteor CRM in the Meteor way',
-    version: '0.0.1',
+    version: '0.0.2',
     git: 'https://github.com/waitingkuo/meteor-crm-client.git'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
 
 
   api.versionsFrom('METEOR@0.9.0');
-  api.use(['coffeescript', 'iron:router@0.9.4', 'ddp@1.0.10', 'tracker@1.0.3'], 'client');
+  api.use(['coffeescript', 'iron:router@0.9.4', 'ddp@1.0.10', 'tracker@1.0.2'], 'client');
   api.addFiles('meteor-crm.coffee', 'client');
   api.export('Crm', 'client');
 
